a8ed24f11658e7319c2ccf523c5e6773b7d49461
Rashed1721 <rashednsu172@gmail.com>
2024-05-19 14:29:11 +0600

    done student features

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,20 @@
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
+import { StudentRoutes } from './app/modules/student/student.route';
 const app: Application = express();
 
 //parser
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
+//application routes
+app.use('/api/v1/students', StudentRoutes);
+
+const getAcontroller = (req: Request, res: Response) => {
   res.send('hello world!!');
-});
+};
+
+app.get('/', getAcontroller);
 
 console.log(process.cwd());
 export default app;
---
+++ src/app/modules/student/student.controller.ts
@@ -0,0 +1,53 @@
+import { Request, Response } from 'express';
+import { StudentServices } from './student.service';
+
+const createStudent = async (req: Request, res: Response) => {
+  try {
+    const { student: studentData } = req.body;
+
+    const result = await StudentServices.createStudentIntoDb(studentData);
+
+    res.status(200).json({
+      success: true,
+      message: 'Student is created successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getAllStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentsFromDb();
+
+    res.status(200).json({
+      success: true,
+      message: 'students are retrived  successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getSingleStudent = async (req: Request, res: Response) => {
+  try {
+    const studentId = req.params.studentId;
+    const result = await StudentServices.getSingleStudentFromDb(studentId);
+
+    res.status(200).json({
+      success: true,
+      message: 'student is retrived successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const StudentControllers = {
+  createStudent,
+  getAllStudents,
+  getSingleStudent,
+};
